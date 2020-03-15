be4e7d9678cb6919c53c47b8ede68e6ce7c0b0f2
Jianzhen Long <u5474453@anu.edu.au>
2020-03-16 01:09:32 +1100

    add title bar

---
+++ rechat/src/components/chatName.js
@@ -0,0 +1,14 @@
+class chatTitle extends React.Component {
+    render() {
+        return (
+            <div style={{backgroundColor:"B2C098",position:"absolute",left:"50%-translateX(50%)",top:"50%-translateY(50%)"}}>
+                {this.props.name}
+            </div>
+        );
+    }
+}
+
+// ReactDOM.render(
+//     <chatTitle name=""/>,
+//     document.getElementById('hello-example')
+// );
