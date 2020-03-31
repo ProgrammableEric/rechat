fc6de054143675f2448ac1a7faf759117c00576d
Jianzhen Long <u5474453@anu.edu.au>
2020-03-31 23:53:30 +1100

    add chatItem

--- rechat/src/components/ChatItem/index.js
+++ rechat/src/components/ChatItem/index.js
@@ -5,11 +5,20 @@ import { ReactComponent as Icon } from './girl.svg';
 
 export default class ChatItem extends Component {
 
+    textDisp(){
+        
+    }
 
     render() {
+        const { id, img, time, name, msg, clickAvatar} = this.props;
         return (
             <div className="wrapper">
-                
+                <div className='chat-item'>
+                    <img name={name} src={img} width='40px' height='40px'/>
+                    <span>{name}</span>
+                    <span>{time}</span>
+                    <div className='message'>this.textDisp(msg)</div>
+                </div>
             </div>
         )
     }
