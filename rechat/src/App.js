272f73e97266acc96e45a52a3579d776fd8c9de2
Jianzhen Long <u5474453@anu.edu.au>
2020-04-07 00:45:21 +1000

    merge update

--- rechat/src/App.js
+++ rechat/src/App.js
@@ -9,11 +9,7 @@ import LeftHeader from './components/LeftHeader';
 import ChatItem from './components/ChatItem';
 import UserAvatar from './components/UserAvatar';
 import MainPageLeft from './components/MainPageLeft';
-<<<<<<< HEAD
-import ChatWind from './components/chatWindow';
-=======
 import PrivateChat from './components/PrivateChat';
->>>>>>> 67ede3b77cce70cb607c48e3f8d247e8496f8b19
 // import ListItem from './components/ListItem'; 
 
 
@@ -24,14 +20,9 @@ function App() {
   const src = `${sex}-${iconIdenx}.png`;
 
   return (
-<<<<<<< HEAD
-    
-    <ChatWind></ChatWind>
-=======
     <div className='wrapper'>
       <PrivateChat />
     </div>
->>>>>>> 67ede3b77cce70cb607c48e3f8d247e8496f8b19
   );
 }
 
