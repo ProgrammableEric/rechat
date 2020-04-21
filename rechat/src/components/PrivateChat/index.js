9be02801efe3595f45bf66f94087221fa6a37a43
Jianzhen Long <u5474453@anu.edu.au>
2020-04-22 00:53:02 +1000

    add moving line but a bug needs to be fixed

--- rechat/src/components/PrivateChat/index.js
+++ rechat/src/components/PrivateChat/index.js
@@ -3,15 +3,45 @@ import './style.css';
 import MainPageLeft from '../../components/MainPageLeft';
 import ChatWind from '../../components/chatWindow';
 
-export default class PrivateChat extends Component {
 
+export default class PrivateChat extends Component {
+    handleDrag(){
+        window.onload = function(){
+            var line = this.document.getElementById("line");
+            var left = this.document.getElementById("left");
+            var right = this.document.getElementById("right");
+            var box = this.document.getElementById("box");
+            line.onmousedown = function(e) {
+                var startX = e.clientX;
+                line.left = line.offsetLeft;
+                this.onmousemove = function (e) {
+                    var endX = e.clientX;
+                    var movement = line.left + (endX - startX);
+                    var maxMov = box.clientWidth -line.offsetWidth;
+                    // add max and min widths for left part here
+
+                    line.style.left = movement;
+                    left.style.width = movement + "px";
+                    right.style.width = (box.clientWidth - movement - 2 ) + "px";
+                }
+                this.onmouseup = function (eve) {
+                    document.onmousemove = null;
+                    document.onmouseup = null;
+                    // line.releasePointerCapture();
+                }
+                //line.setPointerCapture();
+                return false;
+            }
+        }
+    }
 
     render() {
         return (
             <div className="backgroundOverlay">
-                <div className="privateChatWrapper">
-                    <MainPageLeft className="pageLeft"/>
-                    <ChatWind className="pageRight"/>
+                <div className="privateChatWrapper" id="box">
+                    <MainPageLeft id="left"/>
+                    <div id="line" onClick={this.handleDrag()}></div>
+                    <ChatWind id="right"/>
                 </div>
                 
             </div>
